a8074f73a5b3498d049bae2ed7e73b6da913f090
Alexandre Xavier <ale.bxsantos@gmail.com>
2023-06-23 20:08:56 -0300

    test: adds url tests
    Adds tests for the admin paths. Also corrects the pytest configuration in toml file.

---
+++ tests/test_eiger/test_urls.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+from django.test import Client
+
+
+def test_admin_unauthorized(client: Client) -> None:
+    """This test ensures that admin panel requires auth."""
+    response = client.get('/admin/')
+
+    assert response.status_code == HTTPStatus.FOUND
+
+
+def test_admin_authorized(admin_client: Client) -> None:
+    """This test ensures that admin panel is accessible."""
+    response = admin_client.get('/admin/')
+
+    assert response.status_code == HTTPStatus.OK
