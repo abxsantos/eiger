2820555a104a19d236ed802f64bdd85c8b3cd597
Alexandre Xavier <ale.bxsantos@gmail.com>
2023-06-23 20:14:53 -0300

    build(settings.py): adds django-stubs-ext as dependency
    Adds django-stubs-ext as dependency and adjusts settings file to stubs can work.

--- eiger/settings.py
+++ eiger/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 
+import django_stubs_ext
 from decouple import AutoConfig
+
+# Monkeypatching Django, so stubs will work for all generics,
+# see: https://github.com/typeddjango/django-stubs
+django_stubs_ext.monkeypatch()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
