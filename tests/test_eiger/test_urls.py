598b4a209f82ce2c1ab26a84fc5f335dc2b5a194
Alexandre Xavier <ale.bxsantos@gmail.com>
2023-06-23 20:22:13 -0300

    test: ensures admin docs works as expected
    Adds docutils depedency and maps admin docs urls. Ensures admin docs works as expected with tests.

--- tests/test_eiger/test_urls.py
+++ tests/test_eiger/test_urls.py
@@ -15,3 +15,18 @@ def test_admin_authorized(admin_client: Client) -> None:
     response = admin_client.get('/admin/')
 
     assert response.status_code == HTTPStatus.OK
+
+
+def test_admin_docs_unauthorized(client: Client) -> None:
+    """This test ensures that admin panel docs requires auth."""
+    response = client.get('/admin/doc/')
+
+    assert response.status_code == HTTPStatus.FOUND
+
+
+def test_admin_docs_authorized(admin_client: Client) -> None:
+    """This test ensures that admin panel docs are accessible."""
+    response = admin_client.get('/admin/doc/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert b'docutils' not in response.content
