d9b0adccd8026b5182f089a5212875b6c3dcebbf
Alexandre Xavier <ale.bxsantos@gmail.com>
2023-07-06 23:11:53 -0300

    build: changes fly port

--- scripts/gunicorn_config.py
+++ scripts/gunicorn_config.py
@@ -4,7 +4,7 @@
 
 import multiprocessing
 
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:8080'
 # Concerning `workers` setting see:
 # https://github.com/wemake-services/wemake-django-template/issues/1022
 workers = multiprocessing.cpu_count() * 2 + 1
